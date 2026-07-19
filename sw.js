27d5371f928d7ca084e3cd4d39f280c121b33ed3
zachariah-CL <zachariah.zt@gmail.com>
2026-07-19 15:41:19 +0930

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'quiet-log-v6';
+const CACHE = 'quiet-log-v7';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
